d32fa6659ed5b550740424a0e3b22d9145697e13
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-14 11:48:34 +0200

    Add project files.

---
+++ JaggedArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
